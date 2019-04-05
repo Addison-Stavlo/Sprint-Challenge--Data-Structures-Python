709598bcd70a8278e4f04ecec6879cb64760ef97
Addison-Stavlo <AddieStavlo@gmail.com>
2019-04-05 13:00:59 -0400

    O(n) solution for names, answers complete

--- names/names.py
+++ names/names.py
@@ -12,37 +12,49 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 
-# original n^2 solution, 12sec runtime
+# --ORIGINAL SOLUTION--  O(n^2) - 12sec runtime
 # duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-
-
-# stretch solution using only ARRs - 2sec runtime
+# --FIRST PASS UPGRADE-- solution ~ 2sec runtime.
 # duplicates = [i for i in names_2 if i in names_1]
 
+# --STRETCH SOLUTION-- using only arr's.
+# since heap only uses lists/arrays to function
+# O(nlog(n)), 0.4 sec runtime
+# sorted_1 = heap_sort(names_1)
+# sorted_2 = heap_sort(names_2)
 
-# my best solution, O(nlog(n)), 0.4 sec runtime
-sorted_1 = heap_sort(names_1)
-sorted_2 = heap_sort(names_2)
+# duplicates = []
 
+# i = 0
+# j = 0
+
+# while i < len(names_1) and j < len(names_2):
+#     if sorted_1[i] == sorted_2[j]:
+#         duplicates.append(sorted_1[i])
+#         i += 1
+#         j += 1
+#     elif sorted_1[i] < sorted_2[j]:
+#         j += 1
+#     else:
+#         i += 1
+
+
+# -- BEST SOLUTION--, O(n).  hash table for the win.
+# 0.01sec runtime
+names_table = {}
 duplicates = []
+for name in names_1:
+    names_table[name] = True
 
-i = 0
-j = 0
+for name in names_2:
+    if name in names_table:
+        duplicates.append(name)
 
-while i < len(names_1) and j < len(names_2):
-    if sorted_1[i] == sorted_2[j]:
-        duplicates.append(sorted_1[i])
-        i += 1
-        j += 1
-    elif sorted_1[i] < sorted_2[j]:
-        j += 1
-    else:
-        i += 1
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
